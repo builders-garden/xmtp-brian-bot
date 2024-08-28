53a63766100d783d1aa79cbe4a891a68f7c0c0d9
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-08-28 15:40:59 +0200

    add commands and inMemoryCacheStep

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,63 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 import { generateBrianPayload } from "./lib/utils.js";
 import axios from "axios";
-import { brianAgentEndpoint } from "./lib/const.js";
+import { BOT_COMMAND_ACTIONS_REPLY, BOT_COMMAND_HELP_REPLY, BOT_COMMAND_IDLE_REPLY, BOT_COMMAND_RESET_REPLY, BOT_COMMAND_START_REPLY, brianAgentEndpoint } from "./lib/const.js";
+
+// Define the CurrentStep type
+type CurrentStep = 'idle' | 'chatting';
+
+// Create the in-memory cache
+const inMemoryCacheStep = new Map<string, CurrentStep>();
 
 run(async (context: HandlerContext) => {
   // Get the message and the address from the sender
   const { content, sender } = context.message;
+  console.log("Received message from ", sender.address);
 
   if (content.content && typeof content.content === "string") {
-    const brianPayload = await generateBrianPayload(context, 15);
-    const brianHeaders = {
-      "x-brian-api-key": process.env.BRIAN_API_KEY,
-      "Content-Type": "application/json",
-    };
+    const userMessage = content.content.trim().toLowerCase();
+    if (userMessage === "/actions") {
+      await context.send(BOT_COMMAND_ACTIONS_REPLY);
+    } 
+    else if (userMessage === "/help") {
+      await context.send(BOT_COMMAND_HELP_REPLY);
+    } 
+    else if (userMessage === "/start") {
+      inMemoryCacheStep.set(sender.address, 'chatting');
+      await context.send(BOT_COMMAND_START_REPLY);
+    } 
+    else if (userMessage === "/reset") {
+      inMemoryCacheStep.set(sender.address, 'idle');
+      await context.send(BOT_COMMAND_RESET_REPLY);
+    } 
+    else if (inMemoryCacheStep.get(sender.address) === 'chatting') {
+      console.log("Starting conversation with Brian API");
+      const brianPayload = await generateBrianPayload(context, 15);
+      const brianHeaders = {
+        "x-brian-api-key": process.env.BRIAN_API_KEY,
+        "Content-Type": "application/json",
+      };
 
-    // Call the Brian API endpoint with the brianPayload
-    try {
-      const response = await axios.post(brianAgentEndpoint, brianPayload, { headers: brianHeaders });
-      const data = response.data;
+      try {
+        const response = await axios.post(brianAgentEndpoint, brianPayload, { headers: brianHeaders });
+        const data = response.data;
 
-      console.log("Status: ", response.status);
-      console.log("Data: ", data);
+        console.log("Status: ", response.status);
+        console.log("Data: ", data);
 
-      // If the answer is an array, Brian found a possible transaction
-      if (Array.isArray(data.result)) {
-        await context.send(data.result[0].data.description);
+        if (Array.isArray(data.result)) {
+          await context.send(data.result[0].data.description);
+        } else {
+          await context.send(data.result.answer);
+        }
+      } catch (error: any) {
+        console.log("Status: ", error.response.status);
+        console.log("Data: ", error.response.data);
+        await context.send(error.response.data.error);
       }
-
-      // If the answer is not an array, Brian keeps the conversation going
-      else {
-        await context.send(data.result.answer);
-      }
-    } catch (error: any) {
-      console.log("Status: ", error.response.status);
-      console.log("Data: ", error.response.data);
-      await context.send(error.response.data.error);
     }
-
-    //console.log(brianPayload);
-  } else {
-    await context.send("This is not a string");
+    else {
+      await context.send(BOT_COMMAND_IDLE_REPLY);
+    }
   }
 });
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,1 +1,7 @@
 export const brianAgentEndpoint = "https://staging-api.brianknows.org/api/v0/agent";
+export const BOT_COMMAND_ACTIONS_REPLY = "Hey there, I'm Brian Agent. Available commands:\n/start - Start chatting with Brian API\n/actions - Show this list of commands";
+export const BOT_COMMAND_RESET_REPLY = "Conversation reset. Type /start to start a new conversation with Brian Agent.";
+export const BOT_COMMAND_HELP_REPLY = "Hey there, I'm Brian Agent. Available commands:\n/start - Start chatting with Brian API\n/actions - Show this list of commands \n/reset - Reset the conversation with Brian Agent \n/help - Show this list of commands";
+export const BOT_COMMAND_START_REPLY = "Great! You can start chatting with Brian Agent. Type /actions to see the list of actions you can perform.";
+export const BOT_COMMAND_IDLE_REPLY = "Hey there, I'm Brian Agent. Available commands:\n/start - Start chatting with Brian API\n/actions - Show this list of commands";
+
